2a58a0f307f49e953a5a663379b57fef9af36dd0
Jerome Marty <jahrome11@gmail.com>
2010-12-16 13:02:27 +0100

    Added forgotten files

---
+++ orc/orc-stdint.h
@@ -0,0 +1,9 @@
+#ifndef _ORC_ORC_ORC_STDINT_H
+#define _ORC_ORC_ORC_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "orc 0.4.6.1"
+/* generated using gnu compiler gcc (Ubuntu 4.4.3-4ubuntu5) 4.4.3 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
